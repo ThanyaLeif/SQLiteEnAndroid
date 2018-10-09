5a55de66c6f74e34b64eaa6def5a964bd5dbea0d
perro-negro <gi.vega@itsur.edu.mx>
2018-10-09 09:57:37 -0500

    Initial commit

---
+++ app/src/main/java/net/ivanvega/sqliteenandroid/db/Usuario.java
@@ -0,0 +1,67 @@
+package net.ivanvega.sqliteenandroid.db;
+
+class Usuario {
+     int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    String nombre;
+     String telefono;
+     String email;
+
+
+    public Usuario(int id, String nombre, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public Usuario(int id, String nombre, String telefono, String email, String red_social) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.red_social = red_social;
+    }
+
+    public String getRed_social() {
+        return red_social;
+    }
+
+    public void setRed_social(String red_social) {
+        this.red_social = red_social;
+    }
+
+    String red_social;
+}
